89881d431d4805274e509ef94ab046cd9a1207cb
ydclxhltt <ydclxhltt@users.noreply.github.com>
2016-03-14 00:56:18 +0800

    增加引入

--- YooSee/YooSee/AppDelegate.m
+++ YooSee/YooSee/AppDelegate.m
@@ -19,6 +19,7 @@
 #import "UMSocialWechatHandler.h"
 #import "UMSocialQQHandler.h"
 #import "UMSocial.h"
+#import "PAIOUnit.h"
 
 
 @interface AppDelegate ()
@@ -39,7 +40,7 @@
     self.window.rootViewController = splashScreenViewController;
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveRemoteMessage:) name:RECEIVE_REMOTE_MESSAGE object:nil];
-    //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ack_receiveRemoteMessage:) name:ACK_RECEIVE_REMOTE_MESSAGE object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ack_receiveRemoteMessage:) name:ACK_RECEIVE_REMOTE_MESSAGE object:nil];
     
     [UMSocialData setAppKey:UM_APP_KEY];
     [UMSocialSinaSSOHandler openNewSinaSSOWithRedirectURL:SINA_CALLBACK];
@@ -157,7 +158,113 @@
     
 }
 
+- (void)ack_receiveRemoteMessage:(NSNotification *)notification{
+    NSDictionary *parameter = [notification userInfo];
+    int key   = [[parameter valueForKey:@"key"] intValue];
+    int result   = [[parameter valueForKey:@"result"] intValue];
+    NSString *contactId = [parameter valueForKey:@"contactId"];
+    switch(key)
+    {
+         case ACK_RET_GET_DEFENCE_STATE:
+        {
+            dispatch_async(dispatch_get_main_queue(), ^
+            {
+                //                NSString *contactId = @"10000";
+                if(result==1)
+                {
+                    [[FListManager sharedFList] setDefenceStateWithId:contactId type:DEFENCE_STATE_WARNING_PWD];
+                    if([[FListManager sharedFList] getIsClickDefenceStateBtn:contactId])
+                    {
+                        [CommonTool addPopTipWithMessage:@"设备密码错误"];
+                    }
+                    
+                }
+                else if(result==2)
+                {
+                    [[FListManager sharedFList] setDefenceStateWithId:contactId type:DEFENCE_STATE_WARNING_NET];
+                    if([[FListManager sharedFList] getIsClickDefenceStateBtn:contactId])
+                    {
+                        [CommonTool addPopTipWithMessage:@"网络异常"];
+                    }
+                    
+                }
+                else if (result==4)
+                {
+                    [[FListManager sharedFList] setDefenceStateWithId:contactId type:DEFENCE_STATE_NO_PERMISSION];
+                    if([[FListManager sharedFList] getIsClickDefenceStateBtn:contactId])
+                    {
+                        [CommonTool addPopTipWithMessage:@"权限不足"];
+                    }
 
+                }
+                
+                [[FListManager sharedFList] setIsClickDefenceStateBtnWithId:contactId isClick:NO];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshMessage"
+                                                                        object:self
+                                                                      userInfo:nil];
+                });
+                
+            });
+            
+            NSLog(@"ACK_RET_GET_DEFENCE_STATE:%i",result);
+        }
+        break;
+        case ACK_RET_SET_NPCSETTINGS_REMOTE_DEFENCE:
+        {
+            dispatch_async(dispatch_get_main_queue(), ^
+            {
+                if(result==1)
+                {
+                    
+                    [[FListManager sharedFList] setDefenceStateWithId:contactId type:DEFENCE_STATE_WARNING_PWD];
+                    if([[FListManager sharedFList] getIsClickDefenceStateBtn:contactId])
+                    {
+                        [CommonTool addPopTipWithMessage:@"设备密码错误"];
+                    }
+                }
+                else if(result == 2)
+                {
+                    [[FListManager sharedFList] setDefenceStateWithId:contactId type:DEFENCE_STATE_WARNING_NET];
+                    if([[FListManager sharedFList] getIsClickDefenceStateBtn:contactId])
+                    {
+                        [CommonTool addPopTipWithMessage:@"网络异常"];
+                    }
+                }
+                else if (result==4)
+                {
+                    [[FListManager sharedFList] setDefenceStateWithId:contactId type:DEFENCE_STATE_NO_PERMISSION];
+                    if([[FListManager sharedFList] getIsClickDefenceStateBtn:contactId])
+                    {
+                        [CommonTool addPopTipWithMessage:@"权限不足"];
+                    }
+                }
+                else
+                {
+                    ContactDAO *contactDAO = [[ContactDAO alloc] init];
+                    Contact *contact = [contactDAO isContact:contactId];
+                    if(nil!=contact)
+                    {
+                        [[P2PClient sharedClient] getDefenceState:contact.contactId password:contact.contactPassword];
+                    }
+                    
+                }
+                [[FListManager sharedFList] setIsClickDefenceStateBtnWithId:contactId isClick:NO];
+                
+                dispatch_async(dispatch_get_main_queue(), ^
+               {
+                   [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshMessage"
+                        object:self
+                        userInfo:nil];
+               });
+            });
+            DLog(@"ACK_RET_GET_DEFENCE_STATE:%i",result);
+        }
+            break;
+            
+    }
+    
+}
 
 
 //  每次试图切换的时候都会走的方法,用于控制设备的旋转方向.
@@ -193,18 +300,57 @@
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)applicationWillResignActive:(UIApplication *)application
+{
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    if ([[P2PClient sharedClient] p2pCallState] == P2PCALL_STATE_READY)
+    {
+        [[P2PClient sharedClient] setP2pCallState:P2PCALL_STATE_NONE];
+        [[PAIOUnit sharedUnit] stopAudio];
+    }
+    [[P2PClient sharedClient] p2pHungUp];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"EnterBackground" object:nil];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//    UIApplication *app = [UIApplication sharedApplication];
+//    UIBackgroundTaskIdentifier taskID;
+//    taskID = [app beginBackgroundTaskWithExpirationHandler:^{
+//        [[P2PClient sharedClient] p2pDisconnect];
+//        [app endBackgroundTask:taskID];
+//    }];
+//    
+//    if (taskID == UIBackgroundTaskInvalid) {
+//        [[P2PClient sharedClient] p2pDisconnect];
+//        NSLog(@"Failed to start background task!");
+//        return;
+//    }
+     [[P2PClient sharedClient] p2pDisconnect];
+    
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    LoginResult *loginResult = [UDManager getLoginInfo];
+    if(loginResult)
+    {
+        application.applicationIconBadgeNumber = 0;
+        BOOL result = [[P2PClient sharedClient] p2pConnectWithId:loginResult.contactId codeStr1:loginResult.rCode1 codeStr2:loginResult.rCode2];
+        if(result)
+        {
+            NSLog(@"p2pConnect success.");
+        }
+        else
+        {
+            NSLog(@"p2pConnect failure.");
+        }
+    }
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
