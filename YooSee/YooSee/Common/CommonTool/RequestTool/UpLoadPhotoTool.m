c106f7662870400ed9146af2fd467a30604e89a0
ydclxhltt <ydclxhltt@users.noreply.github.com>
2016-03-26 01:18:37 +0800

    上传图片接口修改

--- YooSee/YooSee/Common/CommonTool/RequestTool/UpLoadPhotoTool.m
+++ YooSee/YooSee/Common/CommonTool/RequestTool/UpLoadPhotoTool.m
@@ -10,7 +10,7 @@
 #import "RequestDataTool.h"
 #import "AFNetworking.h"
 
-#define FILE_NAME   @"pic"
+#define FILE_NAME   @"attach"
 
 @interface UpLoadPhotoTool()
 {
@@ -64,10 +64,8 @@
     manager.requestSerializer = [AFHTTPRequestSerializer  serializer];
     [manager.requestSerializer setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     //manager.requestSerializer.timeoutInterval = TIMEOUT;
-    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
     manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html",@"text/json",@"application/json",@"text/plain",nil];
-    self.requestDic = [RequestDataTool makeRequestDictionary:self.requestDic];
-    self.requestDic = @{@"json":[RequestDataTool encryptWithDictionary:self.requestDic]};
     requestOperation =  [manager POST:weakSelf.upLoadUrl parameters:self.requestDic
     constructingBodyWithBlock:^(id<AFMultipartFormData> formData)
     {
@@ -76,22 +74,22 @@
             for (int i = 0; i < [self.photoArray count]; i++)
             {
                 UIImage *image = self.photoArray[i];
-                //NSData *data = UIImageJPEGRepresentation(image, .1);
-                NSData *data = UIImagePNGRepresentation(image);
+                NSData *data = UIImageJPEGRepresentation(image, .4);
+                //NSData *data = UIImagePNGRepresentation(image);
                 NSTimeInterval time = [[NSDate date] timeIntervalSince1970];
                 NSString *nameStr = [NSString stringWithFormat:@"%@%d.jpg",FILE_NAME,i + 1];
                 nameStr = FILE_NAME;
                 nameStr = ([self.photoArray count] == 1) ? FILE_NAME : nameStr;
                 NSString *picName = [NSString stringWithFormat:@"%.0f%d.png",time,i];
-                [formData appendPartWithFileData:data name:nameStr fileName:picName mimeType:@"image/png"];
+                [formData appendPartWithFileData:data name:nameStr fileName:picName mimeType:@"image/jpg"];
             }
 
         }
     }
-    success:^(AFHTTPRequestOperation *operation, id responseObject)
+    success:^(AFHTTPRequestOperation *operation, id responseDic)
     {
         NSLog(@"------%@",operation.responseString);
-        weakSelf.responseDic = [RequestDataTool decryptJSON:operation.responseString];
+        weakSelf.responseDic = responseDic;
         if (weakSelf.delegate && [weakSelf.delegate respondsToSelector:@selector(uploadPhotoSucessed:)])
         {
             [weakSelf.delegate uploadPhotoSucessed:weakSelf];
